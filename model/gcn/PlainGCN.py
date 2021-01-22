62470576059abcd4f6448188f0bc9a2accd552fb
Chenfei Wang <barrycf.wang@gmail.com>
2021-01-22 09:44:36 +0800

    fix bugs in symmetric learning for GCN

--- model/gcn/PlainGCN.py
+++ model/gcn/PlainGCN.py
@@ -1,5 +1,6 @@
 
 
+import torch
 from torch.nn import Sequential as Seq
 from torch.nn import ModuleList
 from .tools import *
@@ -10,16 +11,21 @@ class PlainGCN(torch.nn.Module):
     def __init__(self, model_cfg, input_channels=4):
         super(PlainGCN, self).__init__()
         self.num_filters = model_cfg.NUM_FILTERS
+        self.k = model_cfg.K
         self.act = model_cfg.ACT
         self.norm = model_cfg.NORM
         self.bias = model_cfg.BIAS
         self.dgn = model_cfg.DYN_GRAPH
         self.sum = model_cfg.SUM
         self.diss = model_cfg.DISS
+        self.sym = model_cfg.SYMMETRY
 
+        if self.sym:
+            input_channels = int(input_channels/2)
 
         channels = [input_channels] + self.num_filters
-        self.num_point_features = channels[-1]
+        self.channels = channels
+        self.num_point_features = channels[-1] if not self.sym else channels[-1]*2
         model_list = []
         for i in range(len(channels) - 1):
             in_c = channels[i]
@@ -28,22 +34,37 @@ class PlainGCN(torch.nn.Module):
 
         self.models = ModuleList(model_list)
 
+        if self.sym:
+            model_s_list = []
+            for i in range(len(channels) - 1):
+                in_c = channels[i]
+                out_c = channels[i + 1]
+                model_s_list += [EdgeConv(in_c, out_c, act=self.act, norm=self.norm, bias=self.bias, diss=self.diss)]
+            self.models_s = ModuleList(model_s_list)
+
     def forward(self, batch_dict):
         coords = batch_dict['voxel_coords']
         features = batch_dict['pillar_features'].unsqueeze(-1)
+        if self.sym:
+            feature_o = features[:, :self.channels[0], :]
+            feature_s = features[:, self.channels[0]:, :]
 
         pos = coords[:, 1:4].unsqueeze(-1)
         batch_idx = coords[:, 0].long()
 
-        if not self.dgn:                        # static graph
-            index = knn(pos, batch_idx, k=9)
-            for model in self.models:
-                features = model(features, index, pos)
+        index = knn(pos, batch_idx, k=self.k)
+        if self.sym:
+            for model, model_s in zip(self.models, self.models_s):
+                if self.dgn:
+                    index = knn(feature_o, batch_idx, k=9)
+                feature_o = model(feature_o, index, pos)
+                feature_s = model_s(feature_s, index, pos)
+            features = torch.cat([feature_o, feature_s], dim=1)
         else:
             for model in self.models:
-                index = knn(features, batch_idx, k=9)
+                if self.dgn:
+                    index = knn(features, batch_idx, k=9)
                 features = model(features, index, pos)
-
 
         features = features.squeeze()
 
--- model/vfe/GCNVFE.py
+++ model/vfe/GCNVFE.py
@@ -6,50 +6,6 @@ from ..gcn.tools import *
 from ..gcn.conv import *
 from torch.nn import ModuleList
 
-class PFNLayer(nn.Module):
-    def __init__(self,
-                 in_channels,
-                 out_channels,
-                 use_norm=True,
-                 last_layer=False):
-        super().__init__()
-
-        self.last_vfe = last_layer
-        self.use_norm = use_norm
-        if not self.last_vfe:
-            out_channels = out_channels // 2
-
-        if self.use_norm:
-            self.linear = nn.Linear(in_channels, out_channels, bias=False)
-            self.norm = nn.BatchNorm1d(out_channels, eps=1e-3, momentum=0.01)
-        else:
-            self.linear = nn.Linear(in_channels, out_channels, bias=True)
-
-        self.part = 50000
-
-    def forward(self, inputs):
-        if inputs.shape[0] > self.part:
-            # nn.Linear performs randomly when batch size is too large
-            num_parts = inputs.shape[0] // self.part
-            part_linear_out = [self.linear(inputs[num_part * self.part:(num_part + 1) * self.part])
-                               for num_part in range(num_parts + 1)]
-            x = torch.cat(part_linear_out, dim=0)
-        else:
-
-            x = self.linear(inputs)
-
-        torch.backends.cudnn.enabled = False
-        x = self.norm(x.permute(0, 2, 1)).permute(0, 2, 1) if self.use_norm else x
-        torch.backends.cudnn.enabled = True
-        x = F.relu(x)
-        x_max = torch.max(x, dim=1, keepdim=True)[0]
-        if self.last_vfe:
-            return x_max
-        else:
-            x_repeat = x_max.repeat(1, inputs.shape[1], 1)
-            x_concatenated = torch.cat([x, x_repeat], dim=2)
-            return x_concatenated
-
 
 class GCNVFE(VFETemplate):
     def __init__(self, model_cfg, num_point_features, voxel_size, point_cloud_range, dataset):
@@ -68,14 +24,6 @@ class GCNVFE(VFETemplate):
         assert len(self.num_filters) > 0
         num_filters = [num_point_features] + list(self.num_filters)
 
-        # pfn_layers = []
-        # for i in range(len(num_filters) - 1):
-        #     in_filters = num_filters[i]
-        #     out_filters = num_filters[i + 1]
-        #     pfn_layers.append(
-        #         PFNLayer(in_filters, out_filters, self.use_norm, last_layer=(i >= len(num_filters) - 2))
-        #     )
-        # self.pfn_layers = nn.ModuleList(pfn_layers)
 
         self.voxel_x = voxel_size[0]
         self.voxel_y = voxel_size[1]
